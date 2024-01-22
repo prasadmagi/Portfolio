39d9247b4495587a8da3bfc7d67321fcbf742e24
prasad <prasad.magi@finacus.co.in>
2024-01-22 18:25:21 +0530

    First Commit
    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <h3>Main Application</h3> */}
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
